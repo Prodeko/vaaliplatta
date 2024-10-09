c7982f8f6e106b8c23aca920818aaf4f61558246
Risto Virtaharju <rvirtaha@hotmail.com>
2024-10-09 20:26:50 +0300

    add loading spinner

--- frontend/src/components/Position.tsx
+++ frontend/src/components/Position.tsx
@@ -1,10 +1,13 @@
 import { Position } from "../hooks/useAppState"
+import Loading from "./Loading"
 
 interface PositionProps {
     position: Position | "loading"
 }
 
 export default function PositionView({ position }: PositionProps) {
+    if (position === "loading") return <Loading />
+
     return (
         <div>
             {position?.toString()}
