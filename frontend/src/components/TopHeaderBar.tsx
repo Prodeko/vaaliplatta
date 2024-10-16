e5252cc506afdbe5fd2f6a611fd431b3a2f8d570
Risto Virtaharju <rvirtaha@hotmail.com>
2024-10-16 10:41:22 +0300

    Make TopHeaderBar responsive to position user is viewing

--- frontend/src/components/TopHeaderBar.tsx
+++ frontend/src/components/TopHeaderBar.tsx
@@ -1,10 +1,22 @@
 import { useAppState } from "../hooks/useAppState"
 
 export default function TopHeaderBar() {
-    const { election } = useAppState()
+    const { election, position } = useAppState()
+
+    const text = position && position !== "loading"
+        ? position.name
+        : election?.name || "Vaaliplatta"
+
     return (
         <>
-            <h1 className="text-6xl p-16 font-semibold text-white">{election?.name ?? "Vaaliplatta"}</h1>
+            <h1 className="text-6xl font-semibold text-white
+                p-8
+                sm:p-8
+                md:p-12
+                lg:p-12
+                xl:p-12
+                2xl:p-12
+            ">{text}</h1>
         </>
     )
 }
