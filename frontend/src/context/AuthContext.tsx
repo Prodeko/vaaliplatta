f278594d0782e9b15989a7988968cf46355cbed8
Risto Virtaharju <rvirtaha@hotmail.com>
2024-10-16 22:05:15 +0300

    make user id available in frontend

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -7,14 +7,20 @@ interface AuthProviderProps {
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     const [token, setToken] = useState<Token | null>(localStorage.getItem("token"));
+    const [user, setUser] = useState<string | null>(localStorage.getItem("user"));
 
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
         const token = urlParams.get('token');
+        const user = urlParams.get('user');
 
         if (token) {
             localStorage.setItem('token', token);
             setToken(token)
+        }
+        if (user) {
+            localStorage.setItem('user', user)
+            setUser(user)
         }
     }, [])
 
@@ -26,10 +32,12 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     const logout = () => {
         console.log("logout")
         setToken(null);
+        setUser(null)
         localStorage.removeItem('token');
+        localStorage.removeItem('user')
     };
 
-    const value: AuthContextType = { token, login, logout };
+    const value: AuthContextType = { token, user, login, logout };
 
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
