3caab1175ce73cb8241806a700e48574aa1f1a92
Risto Virtaharju <rvirtaha@hotmail.com>
2024-11-05 11:53:03 +0200

    serve read receipts in the position/:id route

--- backend/src/routers/position.ts
+++ backend/src/routers/position.ts
@@ -4,6 +4,8 @@ import { validateData, validateRouteParams } from "../middleware/validators";
 import { db } from "../database";
 import { jsonArrayFrom } from "kysely/helpers/postgres";
 import { AuthenticatedRequest, requireAuthenticated, requireSuperUser } from "../middleware/auth";
+import { ExpressionBuilder } from "kysely";
+import { DB } from "../db";
 
 const positionRouter = Router();
 
@@ -33,33 +35,45 @@ const idRouteParamsSchema = z.object({
     id: z.string(),
 })
 
-positionRouter.get('/:id', validateRouteParams(idRouteParamsSchema), async (req, res, next) => {
-    const id = parseInt(req.params.id!)
+positionRouter.get('/:id', validateRouteParams(idRouteParamsSchema), async (req: AuthenticatedRequest, res, next) => {
+    try {
+        const id = parseInt(req.params.id!)
+        const user = req.session?.pk || -1
 
-    db
-        .selectFrom("position")
-        .where("id", "=", id)
-        .selectAll()
-        .select(eb => jsonArrayFrom(
+        const applicationsJoinReadReceipts = (eb: ExpressionBuilder<DB, "position">) => jsonArrayFrom(
             eb.selectFrom("application")
+                .leftJoin("read_receipts", join => join
+                    .onRef("application.id", "=", "read_receipts.application_id")
+                    .on("read_receipts.user_id", "=", user.toString())
+                )
                 .selectAll()
-                .whereRef("application.position_id", "=", "position.id")
-        ).as("applications"))
-        .select(eb => jsonArrayFrom(
-            eb.selectFrom("question")
-                .selectAll()
-                .select(eb => jsonArrayFrom(
-                    eb.selectFrom("answer")
-                        .leftJoin("application", "answer.answerer_id", "application.applicant_id")
-                        .select(["answer.id as answer_id", "answer.content as content", "question_id", "answerer_id", "profile_picture", "applicant_name", "applicant_id", "position_id"])
-                        .whereRef("answer.question_id", "=", "question.id")
-                        .where("application.position_id", "=", id)
-                ).as("answers"))
-                .whereRef("question.position_id", "=", "position.id")
-        ).as("questions"))
-        .executeTakeFirst()
-        .then(result => res.status(200).json(result))
-        .catch(err => next(err))
+                .whereRef("application.position_id", "=", "position.id")).as("applications")
+
+
+        const result = await db
+            .selectFrom("position")
+            .where("id", "=", id)
+            .selectAll()
+            .select(applicationsJoinReadReceipts)
+            .select(eb => jsonArrayFrom(
+                eb.selectFrom("question")
+                    .selectAll()
+                    .select(eb => jsonArrayFrom(
+                        eb.selectFrom("answer")
+                            .leftJoin("application", "answer.answerer_id", "application.applicant_id")
+                            .select(["answer.id as answer_id", "answer.content as content", "question_id", "answerer_id", "profile_picture", "applicant_name", "applicant_id", "position_id"])
+                            .whereRef("answer.question_id", "=", "question.id")
+                            .where("application.position_id", "=", id)
+                    ).as("answers"))
+                    .whereRef("question.position_id", "=", "position.id")
+            ).as("questions"))
+            .executeTakeFirst()
+
+        res.status(200).json(result)
+    } catch (error) {
+        next(error)
+    }
+
 })
 
 positionRouter.delete(
