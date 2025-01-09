e4e5e470665d1f15a40cf38f90459cf7edcc5975
Risto Virtaharju <rvirtaha@hotmail.com>
2025-01-09 18:37:50 +0200

    fix: frontend url configuration in dev/prod
    Closes #20

--- frontend/src/hooks/useConfig.ts
+++ frontend/src/hooks/useConfig.ts
@@ -1,9 +1,13 @@
 import { useState } from "react"
+const VITE_BLOB_URL = import.meta.env.VITE_BLOB_URL
+const VITE_API_URL = import.meta.env.VITE_API_URL
+const VITE_LOGIN_URL = import.meta.env.VITE_LOGIN_URL
+const VITE_ROOT_URL = import.meta.env.VITE_ROOT_URL
 
 export default function useConfig() {
-    const [BLOB_URL] = useState<string>('https://vaaliplatta.blob.core.windows.net/prod')
-    const [API_URL] = useState<string>('/api')
-    const [LOGIN_URL] = useState<string>('/oauth2/login')
+    const [BLOB_URL] = useState<string>(VITE_BLOB_URL)
+    const [API_URL] = useState<string>(VITE_ROOT_URL + VITE_API_URL)
+    const [LOGIN_URL] = useState<string>(VITE_ROOT_URL + VITE_LOGIN_URL)
 
     return { BLOB_URL, API_URL, LOGIN_URL }
 }
