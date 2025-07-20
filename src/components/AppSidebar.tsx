fc077cf7a24ca15d6ba57e4f76f4b3e14c6850b6
Risto Virtaharju <rvirtaha@hotmail.com>
2025-07-20 19:06:50 +0300

    feat: add responsive sidebar

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import * as React from "react"
+
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+} from "@/components/ui/sidebar"
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    return (
+        <Sidebar
+            className="top-(--header-height) h-[calc(100svh-var(--header-height))]!"
+            {...props}
+        >
+            <SidebarHeader>
+            </SidebarHeader>
+            <SidebarContent>
+            </SidebarContent>
+            <SidebarFooter>
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,28 +1,24 @@
-import { Logo, LogoText } from "@prodeko/visual-assets/react/logos";
-import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuIndicator,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
+import { Logo } from "@prodeko/visual-assets/react/logos";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { DarkModeToggle } from "./DarkModeToggle";
+import { SidebarTrigger } from "./ui/sidebar";
 
 export default function HeaderBar() {
     return (
-        <header className="flex flex-row shadow-sm shadow-foreground/10 justify-between items-center">
-            <Logo className="h-12 w-auto m-2 text-prodeko-dark dark:text-white" />
-            <p className="text-3xl">Vaaliplatta</p>
-            <div className="flex flex-row gap-2 m-2">
-                <DarkModeToggle className="" />
-                <Avatar className="">
-                    <AvatarImage src="" />
-                    <AvatarFallback>Hi</AvatarFallback>
-                </Avatar>
+        <header className="bg-background sticky top-0 z-50 flex w-full items-center border-b">
+            <div className="flex justify-between h-(--header-height) w-full items-center gap-2">
+                <div className="flex flex-row items-center">
+                    <Logo className="h-12 w-auto m-2 text-prodeko-dark dark:text-white" />
+                    <SidebarTrigger />
+                </div>
+                <p className="text-3xl">Vaaliplatta</p>
+                <div className="flex flex-row gap-2 m-2">
+                    <DarkModeToggle className="" />
+                    <Avatar className="">
+                        <AvatarImage src="" />
+                        <AvatarFallback>Hi</AvatarFallback>
+                    </Avatar>
+                </div>
             </div>
         </header>
     )
