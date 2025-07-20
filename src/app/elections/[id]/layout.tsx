a4903f56bc0027f22426f86c6cbf32b8a138b2eb
Risto Virtaharju <rvirtaha@hotmail.com>
2025-07-20 20:50:22 +0300

    feat: add positions to sidebar

--- src/app/elections/[id]/layout.tsx
+++ src/app/elections/[id]/layout.tsx
@@ -2,15 +2,27 @@ import { AppSidebar } from "@/components/AppSidebar"
 import {
     SidebarInset,
 } from "@/components/ui/sidebar"
+import { db } from "@/lib/kysely"
+import { notFound } from "next/navigation"
 
-export default function RootLayout({
+export default async function RootLayout({
+    params,
     children,
 }: {
+    params: { id: string }
     children: React.ReactNode
 }) {
+    const electionId = Number((await params).id)
+
+    const positions = await db
+        .selectFrom('position')
+        .where('election_id', '=', electionId)
+        .selectAll()
+        .execute()
+
     return (
         <div className="flex flex-1">
-            <AppSidebar />
+            <AppSidebar positions={positions} />
             <SidebarInset>
                 <main className="p-4">
                     {children}
--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -3,13 +3,26 @@
 import * as React from "react"
 
 import {
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
     Sidebar,
     SidebarContent,
     SidebarFooter,
     SidebarHeader,
+    SidebarMenuSub,
+    SidebarMenuSubItem,
+    SidebarMenuSubButton,
 } from "@/components/ui/sidebar"
+import { Position } from "@/lib/db"
+import { Selectable } from "kysely"
+import { groupBy } from "lodash"
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "./ui/collapsible"
+import { ChevronDown } from "lucide-react"
 
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+export function AppSidebar({ positions, ...props }: { positions: Selectable<Position>[], props?: React.ComponentProps<typeof Sidebar> }) {
     return (
         <Sidebar
             className="top-(--header-height) h-[calc(100svh-var(--header-height))]!"
@@ -18,9 +31,55 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <SidebarHeader>
             </SidebarHeader>
             <SidebarContent>
+                <ElectionNav positions={positions} />
             </SidebarContent>
             <SidebarFooter>
             </SidebarFooter>
         </Sidebar>
+    )
+}
+
+export function ElectionNav({
+    positions }: { positions: Selectable<Position>[] }) {
+
+    const { hallitus, vastuutoimarit, toimarit } = groupBy(positions, p => p.category)
+    const categories = [
+        { group: hallitus, label: "HALLITUS" },
+        { group: vastuutoimarit, label: "VASTUUTOIMARIT" },
+        { group: toimarit, label: "TOIMARIT" }
+    ]
+
+    return (
+        <SidebarGroup className="group-data-[collapsible=icon]:hidden">
+            <SidebarGroupLabel>Toimarivirat vuodelle 2026</SidebarGroupLabel>
+            <SidebarMenu>
+                {categories.map((cat) => (
+                    <Collapsible key={cat.label} asChild defaultOpen>
+                        <SidebarMenuItem>
+                            <CollapsibleTrigger asChild>
+                                <SidebarMenuButton>
+                                    <span className="w-full">{cat.label}</span>
+                                    <ChevronDown />
+                                </SidebarMenuButton>
+                            </CollapsibleTrigger>
+
+                            <CollapsibleContent>
+                                <SidebarMenuSub>
+                                    {cat.group.map((subItem) => (
+                                        <SidebarMenuSubItem key={subItem.id}>
+                                            <SidebarMenuSubButton asChild>
+                                                <a>
+                                                    <span>{subItem.name}</span>
+                                                </a>
+                                            </SidebarMenuSubButton>
+                                        </SidebarMenuSubItem>
+                                    ))}
+                                </SidebarMenuSub>
+                            </CollapsibleContent>
+                        </SidebarMenuItem>
+                    </Collapsible>
+                ))}
+            </SidebarMenu>
+        </SidebarGroup>
     )
 }
