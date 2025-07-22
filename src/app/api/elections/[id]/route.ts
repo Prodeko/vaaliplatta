2e6fc2babfe10fca59c0f41a57f9000ce71ee432
Risto Virtaharju <rvirtaha@hotmail.com>
2025-07-22 16:28:11 +0300

    fix: fix api/elections/id route

--- src/app/api/elections/[id]/route.ts
+++ src/app/api/elections/[id]/route.ts
@@ -3,6 +3,6 @@ import { db } from '@/lib/kysely'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-    const rows = await db.selectFrom("election").selectAll().execute()
+    const rows = await db.selectFrom("election").selectAll().executeTakeFirst()
     return NextResponse.json(rows)
 }
